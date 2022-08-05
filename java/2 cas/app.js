bed47d8c481fdc6e2c6c7e00dd23395220098011
mervan <bronjamervan17@gmail.com>
2022-08-05 11:43:57 -0700

    aa

---
+++ java/2 cas/app.js
@@ -0,0 +1,48 @@
+//                             JavaScript Output
+//postoji nekoliko nacina da prikazemo podatke u javascript
+//1. inerHTML
+// Primer:
+document.getElementById("paragraf").innerHTML = "paragraf dodat preko java script-a"
+// 2.document.write() koristi se za testiranje koda.
+// 3. alert() - ova metoda nam salje upozorenje
+// promer:
+// alert("pozdrav");
+//4. console.log() - najcesce koriscena fukcija za prikazivanje stvari u konzoli.
+//primer :
+console.log("pozdrav iz konzole");
+
+//                             JavaScript Identifires/Names
+//Javascript names (imena varijable) moraju pocinjati sa:
+//1. slovom(velikim ili malim)
+//2. dolar sign ($)
+//3. udnerscore (donja crta) _
+// napomena
+// promenjiva ne moze pocinjati brojem. broj se moze naci kao drugi, treci,... karakter u nazivu promenjive.
+
+//                          JavaScript is case sansitive!!!  (Razlikuje velika i mala slova)
+// "a" je razlicito od "A"
+
+//            JavaScript varijable
+//  postoji nekoliko nacina da deklarisemo varijabklu:
+
+
+// 1.Koriscenjem var kljuca reci (keyword)
+var x;   //deklaracija
+x = 10;   //dodele vrednosti promenjivoj x (inicijalizacija)
+var x = 10;  // odjednom izvrsena deklaracija i inicijalizacija
+
+
+
+// 2. koriscenjem let kljucne rei (keyword)
+let x;     //deklaracija
+x = 10;     //dodele vrednosti promenjivoj x (inicijalizacija)
+let x = 10;   // odjednom izvrsena deklaracija i inicijalizacija
+
+
+
+// 3. koriscenjem const kljucne rei (keyword)
+
+const x = 10;   // kod const kljucne reci odjednom se mora izvrsiti deklaracija i inicijalizacija
+
+
+//varijable (promenjive) su kontejneri za skladistenje vrednosti.
