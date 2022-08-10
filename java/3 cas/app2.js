0dc5d54c3bc4ecd3b19fa50ccca8af0a5d257237
mervan <bronjamervan17@gmail.com>
2022-08-10 11:55:41 -0700

    sve

--- java/3 cas/app2.js
+++ java/3 cas/app2.js
@@ -85,4 +85,111 @@ console.log(5*"4");
 console.log(5/"4");
 
 
+// 4.  operatori poredjenja 
+
+// == - Proverava Jednakost  vrednosti
+let r,t;
+r = 56;
+t = -56;
+console.log(r == t);
+
+// === - Proverava jednakost vrednosti i tipa 
+let prom1, prom2;
+prom1 = 14;
+prom2 = "14"
+// console.log(prom1 == prom2);   Bolje je koristiti operator ===
+console.log (prom1 === prom2);
+
+// != - Proverava nejednakost  vrednosti 
+
+let prom3, prom4;
+prom3 = 25;
+prom4 = "25"
+console.log(prom3 != prom4);
+
+// !== - proverava nejednakost vrednosti ili nejdnakost tipa
+let prom5, prom6;
+prom5 = 25;
+prom6 = 25;
+console.log(prom5 !== prom6);
+
+let prom7, prom8;
+prom7 = 25;
+prom8 = "25"
+console.log(prom7 !== prom8);
+
+// > - Promenjiva da li je prva vrednost veca od druge
+
+let prom9,prom10;
+prom9 = 52;
+prom10 = 25;
+console.log(prom9 > prom10);
+
+// < - Promenjiva da li je prva vrednost manja od druge
+
+let prom11,prom12;
+prom9 = 52;
+prom10 = 25;
+console.log(prom11 > prom12);
+
+
+// >= - Promenjiva da li je prva vrednost veca ili jednaka od druge vrednosti
+
+let prom13,prom14;
+prom9 = 50;
+prom10 = 50;
+console.log(prom13 >= prom14);
+
+// <= - Promenjiva da li je prva vrednost manja ili jednaka od druge
+
+let prom15,prom16;
+prom9 = 46;
+prom10 = -46;
+console.log(prom9 <= prom10);
+
+// ? - Ternarni operator
+// sintaksa za njega 
+// condition ? exprIFTrue :   exprIFTrue
+
+
+//5. logicki operatori;
+
+// && - Logicko i
+
+let var1 , var2;
+var1 = 40;
+var2 = 17;
+console.log(var1 > 20  && var2<20);
+console.log(var1>20 && var2>20);
+
+// || - Logicko ili
+let var3, var4;
+var3 = 40;
+var4 = 17;
+console.log(var3 > 20  || var4 < 20);
+
+// ! - Logicnko ne (not)
+
+let isSunny = false;
+console.log(!isSanny);
+
+// 6. tipovni operatori
+
+// typeof - vraca tip promenjive
+
+let var5 = 16.9;
+let var6 = "trenutno je 26 stepeni"
+var isSunny1 = false;
+console.log(typeof var5);
+console.log(typeof var6);
+console.log(typeof isSunny1);
+
+//instanceof - ispituje da li je tip odredjuje promenjice instanca od objekta npr.
+let obj= {
+    ime: "Moje ime ",
+    prezime: "Moje prezime",
+};
+
+console.log(obj instanceof object)
+
 
---
+++ java/4 cas/app.js
@@ -0,0 +1,17 @@
+// za interakciju sa korisnikom se koristi poseban metod imena prompt()
+
+const ime = prompt ("Unesite vase ime:")
+console.log(ime);
+
+let broj_Godine = prompt ("Unesite broj godina:");
+console.log(broj_Godine);
+console.log(typeof broj_Godine)
+//Neki string mozemo prevesti u nubmer na sledeci nacin
+// 1. nacin
+broj_Godine =  +broj_Godine  //preko oznake + ce  se izvrsiti konverzija u nubmer tip podatka
+console.log(typeof broj_Godine)
+// 2. nacin
+broj_Godine = Number (broj_Godine)
+
+let broj_Godine = Number ( prompt ("Unesite broj godina:"));
+console.log(broj_Godine)
